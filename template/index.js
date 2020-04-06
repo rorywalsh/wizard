f50ca50864e2c911c823dc439dc147f77131bd52
rorywalsh <rorywalsh@ear.ie>
2020-04-06 10:49:40 +0100

    testing remote server

--- template/index.js
+++ template/index.js
@@ -16,9 +16,9 @@ Run http-server -c-1 -p80 to start server on open port 80.
 // Network Settings
 // const serverIp      = 'https://yourservername.herokuapp.com';
 // const serverIp      = 'https://yourprojectname.glitch.me';
-const serverIp = '127.0.0.1';
+const serverIp = 'https://https://rorywalsh.github.io/'; //'127.0.0.1';
 const serverPort = '3000';
-const local = true; // true if running locally, false
+const local = false; // true if running locally, false
 // if running on remote server
 
 let playerDetails;

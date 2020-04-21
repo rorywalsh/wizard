94774c6aff5915fbfa9e7cdc94e88680031e2b6e
rorywalsh <rorywalsh@ear.ie>
2020-04-21 09:18:02 +0100

    adding a few more function placeholders to game.state

--- public/card.deck.js
+++ public/card.deck.js
@@ -6,6 +6,8 @@ class Cards {
         this.hands = [];
         this.numberOfCards = 13;
         this.trump = ';'
+
+        //create cards in deck
         this.suits = suits //['red', 'blue', 'green', 'orange'];
         for (var i = 0; i < this.numberOfCards; i++) {
             for (var suit = 0; suits.length < 4; suit++) {
@@ -19,27 +21,26 @@ class Cards {
                 this.cards.push({ suit: this.suits[suit], number: specialCard, shouldDisplay: true });
             }
         }
-
     }
 
     // deal card to players. Returns an array of cards for each player..
     // maybe this should be part of the game state class instead? 
-    dealCards(numCards, numPlayers) {
-        var handsTmp = new Array(numPlayers);
-        if (this.cards.length > numCards) {
-            for (var player = 0; player < numPlayers; player++) {
-                handsTmp[player] = new Array(numCards);
-                for (var i = 0; i < numCards; i++) {
-                    var cardIndex = int(random(0, this.cards.length));
-                    handsTmp[player][i] = (this.cards[cardIndex]);
-                    this.cards.splice(cardIndex, 1);
-                }
-            }
-            this.hands.push(handsTmp);
-            return this.hands
+    // dealCards(numCards, numPlayers) {
+    //     var handsTmp = new Array(numPlayers);
+    //     if (this.cards.length > numCards) {
+    //         for (var player = 0; player < numPlayers; player++) {
+    //             handsTmp[player] = new Array(numCards);
+    //             for (var i = 0; i < numCards; i++) {
+    //                 var cardIndex = int(random(0, this.cards.length));
+    //                 handsTmp[player][i] = (this.cards[cardIndex]);
+    //                 this.cards.splice(cardIndex, 1);
+    //             }
+    //         }
+    //         this.hands.push(handsTmp);
+    //         return this.hands
 
-        } else
-            print("Not enough cards remaining for a deal");
+    //     } else
+    //         print("Not enough cards remaining for a deal");
     }
 
 
--- public/game.state.js
+++ public/game.state.js
@@ -52,6 +52,8 @@ class GameState {
 
     }
 
-
+    getPlayerFromId(id){
+        
+    }
 
 }
